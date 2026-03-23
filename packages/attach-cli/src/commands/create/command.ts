440c9a17655439a3b087ec3190f6e078f4f1729a
Dragos Petri <Dragos.Petri@analog.com>
2026-03-23 15:21:43 +0200

    attach-cli: make create command use the new quick find binding
    Signed-off-by: Dragos Petri <Dragos.Petri@analog.com>

--- packages/attach-cli/src/commands/create/command.ts
+++ packages/attach-cli/src/commands/create/command.ts
@@ -1,10 +1,8 @@
 import { buildCommand } from "@stricli/core";
 
 import * as fs from 'node:fs';
-import path from "node:path";
 
-import { get_all_file_paths } from "../../utilities";
-import { Attach, extract_compatible } from "attach-lib";
+import { find_binding } from "../../utilities";
 
 type Flags = {
     linux: string,
@@ -101,43 +99,3 @@ ${path} {
         }
     }
 });
-
-
-async function find_binding(linux: string, dtSchema: string, compatible_to_find: string): Promise<string | undefined> {
-    const bindings_folder = path.resolve(linux, "Documentation", "devicetree", "bindings");
-
-    if (!fs.existsSync(bindings_folder)) {
-        console.log(`Missing ${bindings_folder}`);
-        return;
-    }
-
-    const all_files = get_all_file_paths(bindings_folder);
-    const yaml_files = all_files.filter(file => file.endsWith(".yaml"));
-
-    for (const file of yaml_files) {
-
-        const attach = Attach.new();
-        const binding = await attach.parse_binding(file, linux, dtSchema);
-
-        if (binding === undefined) {
-            continue;
-        }
-
-        const compatible = extract_compatible(binding.parsed_binding);
-
-        if (compatible === undefined) {
-            continue;
-        }
-
-        for (const entry of compatible) {
-            // TODO: fix why entry could be undefined
-            // arm/actions.yaml
-            if (entry !== undefined && entry === compatible_to_find) {
-                return file;
-            }
-        }
-
-    }
-
-    return;
-}
